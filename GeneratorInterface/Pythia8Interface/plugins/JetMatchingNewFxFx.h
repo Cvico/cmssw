093ff805bd5c8760ba06436e868375a6d5abb6e4
cvico <cvicovillalba1998@gmail.com>
2022-10-03 17:30:40 +0200

    first working version of the plugin to override Pythia's JetMatching

--- GeneratorInterface/Pythia8Interface/plugins/JetMatchingNewFxFx.h
+++ GeneratorInterface/Pythia8Interface/plugins/JetMatchingNewFxFx.h
@@ -1,37 +1,99 @@
 #ifndef GeneratorInterface_Pythia8Interface_JetMatchingNewFxFx_h
 #define GeneratorInterface_Pythia8Interface_JetMatchingNewFxFx_h
 
+// Class declaration for the new JetMatching plugin
+// Author: Carlos Vico (U. Oviedo)
+
+// Includes
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
 #include "FWCore/PluginManager/interface/PluginFactory.h"
 #include "Pythia8/Pythia.h"
+#include "Pythia8Plugins/JetMatching.h"
+#include "Pythia8Plugins/GeneratorInput.h"
 #include <memory>
 #include "GeneratorInterface/Pythia8Interface/interface/CustomHook.h"
 
+// The plugin must inherit from the original Pythia8::JetMatching
+// class
 
-// Adapted by Kevin Pedro to run on cmssw as a user hook
-class JetMatchingNewFxFx : public Pythia8::UserHooks {
-public:
+class JetMatchingNewFxFx : public Pythia8::JetMatching {
+
+public: 
+  // Constructor and destructor
   JetMatchingNewFxFx(const edm::ParameterSet& iConfig);
-  ~JetMatchingNewFxFx() override {}
+  ~JetMatchingNewFxFx(){}
+  
+  // Method declaration
+  bool initAfterBeams();
+  
+  bool canVetoPartonLevelEarly(){return true;}
+  bool doVetoPartonLevelEarly(const Pythia8::Event& event);
 
-  bool initAfterBeams() override;
+  bool canVetoProcessLevel(){return true;}
+  bool doVetoProcessLevel(Pythia8::Event& event);
+   
+  // Shower step vetoes (after the first emission, for Shower-kT scheme)
+  int  numberVetoStep() {return 1;}
+  bool canVetoStep() { return doShowerKt; }
+  bool doVetoStep(int,  int, int, const Pythia8::Event& );
 
-  bool canVetoProcessLevel() override { return true; }
-  bool doVetoProcessLevel(Pythia8::Event& event) override;
-protected:
-  bool setMad_;
-  int scheme_;
-  bool merge_;
-  int jetAlgorithm_;
-  double etaJetMax_;
-  double coneRadius_;
-  int slowJetPower_;
-  double qCut_;
-  bool doFxFx_;
-  double qCutME_;
-  int nQmatch_;
-  int nJetMax_;
+  Pythia8::SlowJet* slowJetDJR;
+
+  Pythia8::vector<double> getDJR() { return DJR;}
+  Pythia8::pair<int,int> nMEpartons() { return nMEpartonsSave;}
+
+  Pythia8::Event getWorkEventJet() { return workEventJetSave; }
+  Pythia8::Event getProcessSubset() { return processSubsetSave; }
+  bool  getExclusive() { return exclusive; }
+  double getPTfirst() { return pTfirstSave; }
+
+
+  // Different steps of the matching algorithm.
+  void sortIncomingProcess(const Pythia8::Event &);
+
+  void jetAlgorithmInput(const Pythia8::Event &, int);
+  void runJetAlgorithm();
+  bool matchPartonsToJets(int);
+  int  matchPartonsToJetsLight();
+  int  matchPartonsToJetsHeavy();
+  int  matchPartonsToJetsOther();
+  bool doShowerKtVeto(double pTfirst);
+
+  // Functions to clear and set the jet clustering scales.
+  void clearDJR() { DJR.resize(0);}
+  void setDJR( const Pythia8::Event& event);
+  // Functions to clear and set the jet clustering scales.
+  void clear_nMEpartons() { nMEpartonsSave.first = nMEpartonsSave.second =-1;}
+  void set_nMEpartons( const int nOrig, const int nMatch) {
+    clear_nMEpartons();
+    nMEpartonsSave.first  = nOrig;
+    nMEpartonsSave.second = nMatch;
+  };
+
+  // Function to get the current number of partons in the Born state, as
+  // read from LHE.
+  int npNLO();
+  
+private:
+  Pythia8::Event processSubsetSave;
+  Pythia8::Event workEventJetSave;
+  double pTfirstSave;
+  
+  bool performVeto;
+
+  Pythia8::vector<int> origTypeIdx[3];
+  int nQmatch;
+  double qCut, qCutSq, clFact;
+  bool doFxFx;
+  int nPartonsNow;
+  double qCutME, qCutMESq;
+
+  Pythia8::vector<double> DJR;
+
+  Pythia8::pair<int, int> nMEpartonsSave;
+
 };
 
+// Register in the UserHook factory
 REGISTER_USERHOOK(JetMatchingNewFxFx);
 #endif
